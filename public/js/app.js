052c82b7adcf19f447b85872207607be890f7cd6
gemma2301 <gemma.s.whitehouse@gmail.com>
2020-07-13 21:39:19 +0200

    updated css report view and landing page

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,31 @@
+var ctx = document.getElementById("myChart");
+Chart.defaults.global.defaultFontFamily = 'Lato';
+Chart.defaults.global.defaultFontSize = 18;
+Chart.defaults.global.defaultFontColor = '#320466';
 
 
-let myChart = document.getElementById('myChart').getContext('2d');
-
-    Chart.defaults.global.defaultFontFamily = 'Lato';
-    Chart.defaults.global.defaultFontSize = 18;
-    Chart.defaults.global.defaultFontColor = '#320466';
-
-
-
-
-let barChart = new Chart(myChart, {
-    type: 'bar', // bar, pie, line, radar 
+var myChart = new Chart(ctx, {
+    type: 'bar',
     data: {
-        labels:['Baseline','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-        datasets:[{
-            label:'Average Pain Level', 
-            data: [0, 2, 4, 5, 4, 9, 4, 5],
-            //backgroundColor: 'purple'
+        labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+        datasets: [{
+            label: 'Average Pain Level',
+            data: [2, 5, 6, 3, 7, 4, 5],
             backgroundColor: ['#4A90E2', '#4A90E2', '#4A90E2', '#4A90E2', '#F093FB', '#4A90E2', '#4A90E2', '#4A90E2'],
-            borderWidth:1,
+            borderWidth: 1,
             borderColor: '#320466'
-
-
         }]
     },
-    options: {}
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    min: 0,
+                    max: 10,
+                    beginAtZero: true
+                    
+                }
+            }]
+        }
+    }
 });
-
-
-
-
